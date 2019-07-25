e1a655556ff4a680a77b56437ca228d8294df9ee
NinaKWelch <ninakwelch@gmail.com>
2019-07-25 18:23:12 +0100

    production build

---
+++ build/precache-manifest.06c956cce68e56e79179e46052021c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926626677d44a9f47ce90aa444359944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af71d14b04f013d078a",
+    "url": "/static/css/main.beb11a55.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "4af71d14b04f013d078a",
+    "url": "/static/js/main.ce8d3c09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.6d80e9d19f36a0cf02acc43061d9aa9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ddd1b72392b03b4ce23817e09a0732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a613b45381e77599dee3",
+    "url": "/static/css/main.beb11a55.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "a613b45381e77599dee3",
+    "url": "/static/js/main.65fabfaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
